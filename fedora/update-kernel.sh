c4122ec3c5cf4f5a3a06e11966ccc66734f0b6e8
Josef Bacik <josef@toxicpanda.com>
2024-02-29 21:09:01 -0500

    add x86.config that we use
    This is used in the CI machines.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fedora/update-kernel.sh
+++ fedora/update-kernel.sh
@@ -32,7 +32,16 @@ EOF
 
 fi
 
-sudo virsh attach-device $1 --live $TMPFILE || _fail "Couldn't attach kernel"
+sudo virsh attach-device $1 --config $TMPFILE || _fail "Couldn't attach kernel"
+sudo virsh shutdown $1
+while [ 1 ]
+do
+	sleep 1
+	sudo virsh list | grep -q $1 || break
+done
+
+sudo virsh start $1
+_wait_for_vm_to_boot $1
 
 read -r -d '' COMMAND << EOM
 ./clean-kernels.sh
@@ -43,7 +52,15 @@ EOM
 
 ssh root@$1 "$COMMAND"
 
-sudo virsh detach-device $1 --live $TMPFILE || _fail "Couldn't detach our device"
+sudo virsh detach-device $1 --config --live $TMPFILE || _fail "Couldn't detach our device"
 rm $TMPFILE
 
 _wait_for_vm_to_boot $1
+
+while [ 1 ]
+do
+        sleep 1
+        ssh root@$1 systemctl status actions.runner.btrfs-linux.$1.service && break
+        ssh root@$1 systemctl stop actions.runner.btrfs-linux.$1.service
+        ssh root@$1 systemctl start actions.runner.btrfs-linux.$1.service
+done
--- fedora/update-xfstests.sh
+++ fedora/update-xfstests.sh
@@ -8,7 +8,21 @@ cd $(dirname $MYPATH)
 
 [ "$#" -ne 1 ] && _fail "must specify a vm name"
 
-read -r -d '' COMMAND << EOM
+if [ "$1" != "xfstests10" ]
+then
+	read -r -d '' COMMAND << EOM
+cd fstests
+git reset --hard
+git checkout master
+git branch -D staging
+git pull
+git checkout staging
+make
+cp btrfs-ci.config local.config
+EOM
+
+else
+	read -r -d '' COMMAND << EOM
 cd fstests
 git reset --hard
 git checkout master
@@ -17,5 +31,7 @@ git pull
 git checkout staging
 make
 EOM
+fi
+
 
 ssh root@$1 "$COMMAND"
